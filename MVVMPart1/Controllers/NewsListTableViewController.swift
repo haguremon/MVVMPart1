e3644b0e1d2b8740ed010fcdfad0fcbd707fef0b
Yuta iwasaki <yanyong0127@gmail.com>
2021-08-22 01:00:58 +0900

    明日復習して作ってみる

--- MVVMPart1/Controllers/NewsListTableViewController.swift
+++ MVVMPart1/Controllers/NewsListTableViewController.swift
@@ -22,9 +22,12 @@ class NewsListTableViewController: UITableViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         let url = URL(string:"https://newsapi.org/v2/top-headlines?country=jp&apiKey=d350da23f44e42ffac319622948514d8")!
         webservice().getarticles(url: url) { [weak self] articles in
+            //articlesは[Article]?を引数にとるClosure
             guard let articles = articles else { return }
+            
             //配列の情報を取得して初期化
             self?.articleListVM = ArticleListViewModel(articles: articles)
+            
             //取得できた時と同時にreloadDataする
             DispatchQueue.main.async {
                 self?.tableView.reloadData()
@@ -36,6 +39,7 @@ class NewsListTableViewController: UITableViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         
+        //articleListVMがからのときに0を入れる
         return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
         
     }
@@ -47,8 +51,9 @@ class NewsListTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ArticleTableViewCell
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ArticleTableViewCell
+        //articleAtIndexがArticleViewModelを返すので特定の記事を取得して表示することができる
         let article = articleListVM.articleAtIndex(indexPath.row)
         
         cell.titleLable.text = article.title
